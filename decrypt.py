4f9ad30bf62204f2ec492a84ca2260ba2108e360
lampubolep <chieldesickn@gmail.com>
2019-11-19 12:56:08 +0700

    Decrypt cipher

---
+++ decrypt.py
@@ -0,0 +1,23 @@
+def decrypt(text, s):
+	result = ""
+	
+	for i in range(len(text)):
+		char = text[i]
+
+		if (char.isupper()):
+			result += chr((ord(char) + 65-s) % 26 + 65)
+		else:
+			result += chr((ord(char) + 97-s) % 26 + 97)
+	print("", end='')
+	return result
+
+print ("Enter text to decrypt")
+text = str(input())
+print ("Enter shift character (int)")
+s = int(input())
+
+print ("==============")
+print ("A =", decrypt("A", s))
+print ("Shift : ", str(s))
+print ("Text : ", text)
+print ("Uncipher: ", decrypt(text,s))
